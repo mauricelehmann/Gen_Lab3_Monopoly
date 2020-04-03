ac1ec367755c55a5586671946cafdb4f67d63cc0
LucasGianinetti <lucas.gianinetti@heig-vd.ch>
2020-04-03 13:39:06 +0200

    .

---
+++ src/test/java/MonopolyGameSystemTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MonopolyGameSystemTest {
+
+    @Test
+    public void Test(){
+        assertEquals(1,2);
+    }
+}
