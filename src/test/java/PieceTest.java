1861ee6960495881098b1fd7da46f9519dc0ba55
Grimlix <nic.hungi5@gmail.com>
2020-04-06 15:13:51 +0200

    Correction bug de package

--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -1,4 +1,15 @@
-package java;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceTest {
+
+
+    @Test
+    void firstPieceTest(){
+        assertEquals(true,true);
+    }
+
+
+
 }
